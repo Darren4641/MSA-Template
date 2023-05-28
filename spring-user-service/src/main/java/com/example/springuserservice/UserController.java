dff0e9c27f10b906ad984e32642dc84539d306d4
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-05-28 23:55:31 +0900

    Eureka서버와 Gateway연동

--- spring-user-service/src/main/java/com/example/springuserservice/UserController.java
+++ spring-user-service/src/main/java/com/example/springuserservice/UserController.java
@@ -1,9 +1,12 @@
 package com.example.springuserservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class UserController {
@@ -11,5 +14,11 @@ public class UserController {
     @GetMapping("/do")
     public String doIt() {
         return "test";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return header + " is Header";
     }
 }
