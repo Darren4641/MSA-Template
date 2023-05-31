5d23915066a85bd66c94cd87cf89c626a3f1defe
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-05-31 15:51:34 +0900

    feat : gateway add security filter

--- spring-api-gateway/src/main/java/com/example/springapigateway/SpringApiGatewayApplication.java
+++ spring-api-gateway/src/main/java/com/example/springapigateway/SpringApiGatewayApplication.java
@@ -3,6 +3,9 @@ package com.example.springapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 public class SpringApiGatewayApplication {
@@ -11,4 +14,8 @@ public class SpringApiGatewayApplication {
         SpringApplication.run(SpringApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity security) {
+        return security.csrf().disable().build();
+    }
 }
